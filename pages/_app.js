98c75239a5b5f116a16f4342cc5bb2e0f90a9d77
skinshy <yyshgk12@gmail.com>
2020-07-20 09:54:56 +0900

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import {wrapper} from "../store";
 import dashboard from "../store/actions/dashboard";
-import 'fontsource-roboto';
+import Layout from '../components/layout';
+import Head from "next/head";
+// import 'fontsource-roboto';
 
 const MyApp = ({Component, pageProps}) => (
-  <Component {...pageProps}/>
+  <>
+    <Head>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    </Head>
+    <Layout>
+      <Component {...pageProps}/>
+    </Layout>
+  </>
 );
 
 MyApp.getInitialProps = async ({Component, ctx}) => {
